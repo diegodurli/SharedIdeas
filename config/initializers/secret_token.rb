8facc0554d61fa10498725c2e665abf7e30d7bab
Diego Durli <dgodurli@gmail.com>
2012-08-01 01:20:11 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedIdeas::Application.config.secret_token = '743333c90d2ca2cea287c7a769a0a2abde0e015ff7bdde9dcfa4b07aafcba137ac93246ab7b1eca3d3e21fc16190b1813f500ccd616224ccf2b80edb9d36e2c2'
